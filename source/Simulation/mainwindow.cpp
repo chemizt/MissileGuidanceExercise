d4596e58ef3b0e04228b0d10f8c1184953b3e71c
Michael Slinkin <michaelnslinkin@gmail.com>
2023-02-12 03:12:20 +0300

    and ANOTHER ONE

--- source/Simulation/mainwindow.cpp
+++ source/Simulation/mainwindow.cpp
@@ -7,7 +7,7 @@
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
-	setWindowIcon(QIcon(":icon/icon.ico"));
+	setWindowIcon(QIcon(":/icon.ico"));
 
 	ui->plot->legend->setVisible(true);
 
