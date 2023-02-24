1a259c56524874cae2c8c5abef7c12293383be2d
Michael Slinkin <michaelnslinkin@gmail.com>
2023-02-24 20:31:50 +0000

    oingo boingo brothers

--- include/Simulation/simulation.hpp
+++ include/Simulation/simulation.hpp
@@ -21,11 +21,12 @@ class Simulation
 		void iterate();
 		void setFileOutputNeededTo(const bool newVal);
 		const double getMslProxyRadius() { return _missile->getProxyRadius(); };
-		void restoreSimObjects()
+		void restoreSimState()
 		{
 			if (_missile) _missile->restore();
 			if (_target) _target->restore();
 			if (_target && _missile) _missile->setTarget(_target);
+			_simElapsedTime = 0.;
 		};
 
 	private:
