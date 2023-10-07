adef637443442a5e753e88cfe8a7fefb1ac99c6d
Michael Slinkin <michaelnslinkin@gmail.com>
2023-10-07 18:18:51 +0000

    - C4305 fixes; - deployment fixes

--- include/Simulation/CommonSimParams.hpp
+++ include/Simulation/CommonSimParams.hpp
@@ -3,10 +3,10 @@
 
 #include <QDebug>
 
-#define AIR_DENSITY		1.225			// плотность воздуха
-#define FREEFALL_ACC	9.80665			// ускорение свободного падения
-#define SIM_RESOLUTION	0.01			// разрешение симуляции
-#define SPEED_OF_SOUND	343.			// скорость звука
+#define AIR_DENSITY		1.225f			// плотность воздуха
+#define FREEFALL_ACC	9.80665f		// ускорение свободного падения
+#define SIM_RESOLUTION	0.01f			// разрешение симуляции
+#define SPEED_OF_SOUND	343.f			// скорость звука
 #define OUT_FILE_NAME	"outputData.csv"
 
 #endif // SIMULATION_PARAMETERS_HDR_IG
